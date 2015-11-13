2ca7cfabce787c3ca9e81984d9defa8bea6e4e8e
ledom <ledom.net+github@gmail.com>
2015-11-13 23:21:14 +0100

    01 done

--- spec/01_let.spec.js
+++ spec/01_let.spec.js
@@ -1,10 +1,12 @@
 describe('LET', () => {
 
-  xit('can be used in place of `var`', () => {
+  it('can be used in place of `var`', () => {
 
     //Declare 'bandName' using 'let'
+    let bandName = "Queen";
 
     //Declare 'isBestBand' using 'let'
+    let isBestBand = true;
 
 
     expect(bandName).toBe("Queen");
@@ -12,33 +14,35 @@ describe('LET', () => {
   });
 
 
-  xit('can modify the value of a `let` variable', () => {
+  it('can modify the value of a `let` variable', () => {
 
     //Delcare 'releaseName' using 'let', setting the value to 'ES6'
+    let releaseName = 'ES6';
 
     //Change value of 'releaseName' to be `ES2015`, the new name for ES6
+    releaseName = 'ES2015';
 
 
     expect(releaseName).toBe("ES2015");
   });
 
 
-  xit('is trapped inside of an `if` statement', () => {
+  it('is trapped inside of an `if` statement', () => {
 
     if(true){
       //Change to `var` to `let`, so that 'b' is scoped inside of the if-statement
-      var b = 1;
+      let b = 1;
     }
 
     expect(()=> console.log(b)).toThrowError(ReferenceError, "Can't find variable: b");
   });
 
 
-  xit('prevents loop counters from hoisting', () => {
+  it('prevents loop counters from hoisting', () => {
 
     function doLoop(){
       //Change loop counter to `let` so that it is trapped inside of the loop, and can't be returned.
-      for(var i = 0; i < 10; i++){
+      for(let i = 0; i < 10; i++){
 
       }
       return i;
@@ -48,12 +52,12 @@ describe('LET', () => {
   });
 
 
-  xit('means that we can start using block statements', () => {
+  it('means that we can start using block statements', () => {
 
     //BLOCK STATEMENT
     {
       //Change to `let` declaration
-      var d = 2;
+      let d = 2;
     }
 
     expect(()=> console.log('d',d)).toThrowError(ReferenceError, "Can't find variable: d");
