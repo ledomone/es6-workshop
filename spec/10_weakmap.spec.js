bf91a219205e766cc2c25b4a97fc6dfb8bb25539
ledom <ledom.net+github@gmail.com>
2015-11-27 00:03:14 +0100

    10 done

--- spec/10_weakmap.spec.js
+++ spec/10_weakmap.spec.js
@@ -1,22 +1,37 @@
 describe('WEAKMAPS', () => {
 
-  xit('has a set method', ()=>{
+  it('has a set method', ()=>{
     let key = {name: "Aaron"};
     let value = {twitter: '@js_dev', gplus: '+AaronFrost'};
 
     // Create a new WeakMap called 'myMap'
     // Add a new entry. Use key as the key and values as the value
-
+    let myMap = new WeakMap();
+    myMap.set(key, value);
 
     expect(myMap.has(key)).toBe(true);
     expect(myMap.get(key)).toEqual(value);
 
   });
 
-  xit('EXTRA CREDIT', () => {
+  it('EXTRA CREDIT', () => {
 
     //If you make it this far, write a class with private member variables, using WeakMaps
-
+    class MyClass {
+      constructor(init) {
+        this._wm = new WeakMap(init)
+      }
+      get(k) {
+        return this._wm.get(k)
+      }
+      has(k) {
+        return this._wm.has(k)
+      }
+      set(k, v) {
+        this._wm.set(k, v)
+        return this
+      }
+    }
   })
 
 });
